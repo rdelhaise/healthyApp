0e45ee2d78431889db949677e7ca77c18932e4c3
Rémy <remydelhaisepro@gmail.com>
2019-05-25 23:31:01 +0200

    Creation de la page patient

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-require "vendor/autoload.php";
+require_once "vendor/autoload.php";
 
 include "templates/header.php";
 include "templates/nav.php";
-include "public/dietitian.php";
+include "public/profiles/patient.php";
 include "templates/footer.php";
---
+++ public/profiles/patient.php
@@ -0,0 +1,75 @@
+<div class="row" style="margin: 0px!important">
+    <!-- Begin Page Content -->
+    <div class="container-fluid col-xl-9 col-lg-9" id="meals">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3 bg-general-color">
+                <h3 class="m-0 font-weight-bold text-white">Name Patient's Profile</h3>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-xl-8">
+                        <div class="row py-4">
+                            <span class="col-xl-6">Date Begin : </span>
+                            <div class="col-xl-6"> <span class="float-right">Diet Type</span></div>
+                        </div>
+                        <h4>General Informations</h4>
+                            Date Birth : <br/>
+                            E-mail : <br/>
+                            <div class="row">
+                                <span class="col-xl-6"> Phone number : </span>
+                                <span class="col-xl-6"> Mobile : </span>
+                            </div>
+                            <div class="row">
+                                <span class="col-xl-6"> Gender : </span>
+                                <span class="col-xl-6"> Smoking : </span>
+                            </div>
+                        <hr class="w-100">
+                        <h4>Address Informations</h4>
+                            Address : <br/>
+                            <div class="row">
+                                <span class="col-xl-6"> City : </span>
+                                <span class="col-xl-6"> ZipCode : </span>
+                            </div>
+                            Country : <br/>
+                        <hr class="w-100">
+                        <h4>Body Informations</h4>
+                        <p>
+                            <div class="row">
+                            <span class="col-xl-6">
+                                Height:
+                            </span>
+                            <span class="col-xl-6">
+                                Weight :
+                            </span>
+                        </div>
+                            Measurements : (Hanche, Taille, Jambes)<br/>
+                        </p>
+                        <hr class="w-100">
+                        <h4>Feeding Informations :</h4>
+                        <p>
+                            Food Type : <br/>
+                            Allergy : <br/>
+                        </p>
+                        <hr class="w-100">
+                        <h4>Family History</h4>
+                        <p></p>
+                        <hr class="w-100">
+                        <h4>Sport<br/></h4>
+                        <p></p>
+                        <hr class="w-100">
+                        <h4>Diseases :</h4>
+                        <p></p>
+                    </div>
+                    <div class="col-xl-4">
+                        <img class="img-fluid rounded" src="http://mangeteslegumes.net/wp-content/uploads/2016/09/IMG_02491.jpg"/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>-
+
+
+</div>
