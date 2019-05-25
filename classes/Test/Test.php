9f1a86e0132b0eeb84dda781993779433ec3ce90
Rémy <remydelhaisepro@gmail.com>
2019-05-25 17:33:13 +0200

    N0OUVELLE APP

---
+++ classes/Test/Test.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Test;
+
+class Test{
+    public function __construct()
+    {
+        echo "test";
+    }
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require "vendor/autoload.php";
+use App\Test\Test;
+new Test();
+/**
+ * Created by PhpStorm.
+ * User: remy
+ * Date: 25/05/2019
+ * Time: 17:10
+ */
