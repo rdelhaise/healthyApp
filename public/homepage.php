9bdaabe98f6759ff650bff6a0e3342207de4335b
Rémy <remydelhaisepro@gmail.com>
2019-05-25 21:34:07 +0200

    Ajout du diet profile sur la homepage, photo de la diet et changement couleur des card head

--- public/homepage.php
+++ public/homepage.php
@@ -1,65 +1,82 @@
-
-
-    <div class="row" style="margin: 0px!important">
+<div class="row" style="margin: 0px!important">
     <!-- Begin Page Content -->
-        <div class="container-fluid col-xl-8 col-lg-8" id="meals">
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Meals</h6>
+    <div class="container-fluid col-xl-9 col-lg-9" id="meals">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3 bg-general-color">
+                <h5 class="m-0 font-weight-bold text-white">Meals</h5>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Office</th>
+                            <th>Age</th>
+                            <th>Start date</th>
+                            <th>Salary</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Tiger Nixon</td>
+                            <td>System Architect</td>
+                            <td>Edinburgh</td>
+                            <td>61</td>
+                            <td>2011/04/25</td>
+                            <td>$320,800</td>
+                        </tr>
+                        <tr>
+                            <td>Donna Snider</td>
+                            <td>Customer Support</td>
+                            <td>New York</td>
+                            <td>27</td>
+                            <td>2011/01/25</td>
+                            <td>$112,000</td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                            <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Position</th>
-                                <th>Office</th>
-                                <th>Age</th>
-                                <th>Start date</th>
-                                <th>Salary</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>Tiger Nixon</td>
-                                <td>System Architect</td>
-                                <td>Edinburgh</td>
-                                <td>61</td>
-                                <td>2011/04/25</td>
-                                <td>$320,800</td>
-                            </tr>
-                            <tr>
-                                <td>Donna Snider</td>
-                                <td>Customer Support</td>
-                                <td>New York</td>
-                                <td>27</td>
-                                <td>2011/01/25</td>
-                                <td>$112,000</td>
-                            </tr>
-                            </tbody>
-                        </table>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+
+    <div class="container-fluid col-xl-3 col-lg-3" id="dietProfile">
+        <a href="#">
+            <div class="card shadow mb-4 ">
+                <div class="card-header py-3 bg-general-color">
+                    <h5 class="m-0 font-weight-bold text-white">Dietitian Profile</h5>
+                </div>
+                <div class="card-body row">
+                    <div class="col-xl-12" id="img-diet">
+                        <img src="http://mangeteslegumes.net/wp-content/uploads/2016/09/IMG_02491.jpg" class=" mx-auto d-block img-fluid rounded" width="100" height="100"/>
                     </div>
+                    <hr class="w-100">
+                    <div class="col-xl-12" id="bodyDiet">
+                        Name Firstname <br/>
+                        Last Appointment : <br/>
+                        Address : <br/>
+                        E-mail : <br/>
+                        Tel. : <br/>
+                        Mobile : <br/>
+                    </div>
+
+
                 </div>
-            </div>
-            <!-- /.container-fluid -->
-
-        </div>
-        <div class="container-fluid col-xl-4 col-lg-4">
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Dietician</h6>
-                </div>
-                <div class="card-body">
 
             </div>
-            <!-- /.container-fluid -->
-        </div>
+        </a>
+        <!-- /.container-fluid -->
+    </div>
 
-    </div>
-    </div>
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
+
+</div>
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
 
