f398c38d1da26e88faf7cf520bd11691c2bd8307
rdelhaise <rdelhaise@misyl-services.coml>
2020-02-09 17:55:55 +0100

    now

--- src/Views/Error404.js
+++ src/Views/Error404.js
@@ -1,9 +1,39 @@
 import React from "react";
-
+import history from "../Components/history";
 const Error404 = () => {
+  /*
+    .loginForm {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+    */
+
   return (
-    <div>
-      <img src="/img/medor.png" />
+    <div
+      className="container primary"
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)"
+      }}
+    >
+      <div className="row ">
+        <h4 className="mx-auto">
+          Êtes-vous perdu ? Médor peut vous aider ! <br />
+          Cliquez lui dessus pour retourner à l'accueil.
+        </h4>
+      </div>
+      <img
+        src="/img/medor.png"
+        className="rounded mx-auto d-block"
+        style={{ cursor: "pointer" }}
+        height="500"
+        alt="404"
+        onClick={() => history.push("/")}
+      />
     </div>
   );
 };
