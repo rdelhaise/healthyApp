3668ad81f488b226cb3bcaec63fa67eefd220d4a
rdelhaise <rdelhaise@misyl-services.coml>
2020-04-09 23:35:07 +0200

    Home beau

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,21 +2,22 @@ import React, { Component } from "react";
 import Menu from "./Menu";
 import UserService from "../Services/User/UserService";
 import Loader from "./Loader";
+import "../Assets/Home/Home.css";
 
 export class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       users: [],
-      patient: []
+      patient: [],
     };
   }
 
   componentDidMount() {
     const storage = JSON.parse(localStorage.getItem("authenticate"));
     if (storage.role === "0") {
-      UserService.getUsers().then(data => {
-        this.setState(state => (state.users = data));
+      UserService.getUsers().then((data) => {
+        this.setState((state) => (state.users = data));
       });
     }
   }
@@ -37,7 +38,7 @@ export class Home extends Component {
                     </tr>
                   </thead>
                   <tbody>
-                    {this.state.users.map(user => (
+                    {this.state.users.map((user) => (
                       <tr key={user.id} style={{ cursor: "pointer" }}>
                         <td>{user.email}</td>
                         <td>{user.password}</td>
@@ -55,109 +56,123 @@ export class Home extends Component {
             </>
           )}
 
-      <div className="content-wrapper">
-        <div className="row proBanner">
-          <div className="col-12">
-            <span className="d-flex align-items-center purchase-popup">
-              <i className="mdi mdi-close bannerClose" onClick={this.toggleProBanner}></i>
-            </span>
-          </div>
-        </div>
-        <div className="col-xl-12 col-lg-6 col-sm-6 grid-margin stretch-card">
-          <div className="row my-5">
-            <div className="card">
-                  <div className="card-body alert-primary">
-                  <h3>Bienvenue sur votre HomePage, voici votre tableau de bord </h3>
-              </div>
+          <div className="dashboard-wrapper">
+            <div className="col-xl-12 col-lg-6 col-sm-6 my-5">
+              <h3 className="text-center">Dashboard </h3>
             </div>
-          </div>
-        </div>
+            <hr />
 
-        <div className="row my-5">
-          <div className="col-xl-4 col-lg-6 col-sm-6 grid-margin stretch-card">
-            <div className="card">
-              <div className="card-body alert-info border border-dark">
-                <h4 className="card-title">Date du jour</h4>
-                <div className="shedule-list d-xl-flex align-items-center justify-content-between mb-3">
-                  <h3>11 Avril 2020</h3>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 col-sm-6 grid-margin stretch-card">
-            <div className="card">
-              <div className="card-body alert-info border border-dark">
-                <h4 className="card-title">Prochain rendez-vous</h4>
-                <div className="shedule-list d-xl-flex align-items-center justify-content-between mb-3">
-                  <h3>09 Mai 2020</h3>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row my-5">
-          <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 grid-margin stretch-card ">
-            <div className="card card-statistics alert-info border border-dark">
-              <div className="card-body ">
-                <div className="clearfix">
-                  <div className="float-left">
-                    <i class="fas fa-user-friends fa-4x"></i>
-                  </div>
-                  <div className="float-right">
-                    <p className="mb-0 text-right text-dark">Patients</p>
-                    <div className="fluid-container">
-                      <h3 className="font-weight-medium text-right mb-0 text-dark">63</h3>
+            <div className="row my-5">
+              <div className="col-xl-4 col-lg-6 col-sm-6">
+                <div className="card">
+                  <div className="card-body">
+                    <h4 className="card-title">Date du jour</h4>
+                    <div className="shedule-list d-xl-flex align-items-center justify-content-between mb-3">
+                      <h3>11 Avril 2020</h3>
                     </div>
                   </div>
                 </div>
-                <p className="text-muted mt-3 mb-0">
-                  <i className="mdi mdi-alert-octagon mr-1" aria-hidden="true"></i> 65% lower growth </p>
               </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 grid-margin stretch-card">
-            <div className="card card-statistics alert-info border border-dark">
-              <div className="card-body">
-                <div className="clearfix">
-                  <div className="float-left">
-                    <i className="mdi mdi-receipt text-warning icon-lg"></i>
-                  </div>
-                  <div className="float-right">
-                    <p className="mb-0 text-right text-dark">Objectif</p>
-                    <div className="fluid-container">
-                      <h3 className="font-weight-medium text-right mb-0 text-dark">70 kg</h3>
+
+              {JSON.parse(localStorage.getItem("authenticate")).role === "2" ? (
+                <>
+                  <div className="col-xl-4 col-lg-6 col-sm-6">
+                    <div className="card">
+                      <div className="card-body">
+                        <div className="float-left">
+                          <h4 className="card-title text-secondary">
+                            Dernier poids
+                          </h4>
+                          <div className="shedule-list d-xl-flex align-items-center justify-content-between mb-3">
+                            <h3 className="text-secondary font-weight-bold">
+                              74 kg
+                            </h3>
+                          </div>
+                        </div>
+                        <div className="float-right">
+                          <h4 className="card-title text-success">Objectif</h4>
+                          <div className="shedule-list d-xl-flex align-items-center justify-content-between mb-3">
+                            <h3 className="text-success font-weight-bold">
+                              70 kg
+                            </h3>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <p className="text-muted mt-3 mb-0">
-                  <i className="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i> Product-wise sales </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 grid-margin stretch-card">
-            <div className="card card-statistics alert-info border border-dark">
-              <div className="card-body">
-                <div className="clearfix">
-                  <div className="float-left">
-                    <i className="mdi mdi-poll-box text-success icon-lg"></i>
-                  </div>
-                  <div className="float-right">
-                    <p className="mb-0 text-right text-dark">Sales</p>
-                    <div className="fluid-container">
-                      <h3 className="font-weight-medium text-right mb-0 text-dark">5693</h3>
+                </>
+              ) : null}
+              {JSON.parse(localStorage.getItem("authenticate")).role ===
+                "1" && (
+                <>
+                  <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 ">
+                    <div className="card">
+                      <div className="card-body ">
+                        <h4 className="card-title">
+                          <i className="fas fa-user-friends fa-1x"></i>
+                          <span className="mx-4">Patients</span>
+                        </h4>
+                        <div className="shedule-list d-xl-flex align-items-center justify-content-start mb-3 ">
+                          <h3 className="text-right">63</h3>
+                        </div>
+                      </div>
                     </div>
                   </div>
+                </>
+              )}
+              <div className={"col-xl-4 col-lg-6 col-sm-6"}>
+                <div
+                  className={
+                    JSON.parse(localStorage.getItem("authenticate")).role ===
+                    "1"
+                      ? "card click-card"
+                      : "card"
+                  }
+                >
+                  <div className="card-body">
+                    <h4 className="card-title">Prochain rendez-vous</h4>
+                    <div className="shedule-list d-xl-flex align-items-center justify-content-between mb-3">
+                      <h3 className="text-success">09 Mai 2020</h3>
+                    </div>
+                  </div>
+                  {JSON.parse(localStorage.getItem("authenticate")).role ===
+                    "1" && (
+                    <div className="card-footer shedule-list d-xl-flex align-items-center justify-content-center ">
+                      <span className="">
+                        Accéder au patient du rendez-vous
+                      </span>
+                    </div>
+                  )}
                 </div>
-                <p className="text-muted mt-3 mb-0">
-                  <i className="mdi mdi-calendar mr-1" aria-hidden="true"></i> Weekly Sales </p>
               </div>
             </div>
+            <div className="row my-5"></div>
           </div>
-        </div>
-        </div>
+          {JSON.parse(localStorage.getItem("authenticate")).role === "2" && (
+            <>
+              <div className="col-xl-12 col-lg-6 col-sm-6 my-5">
+                <h3 className="text-center">Meals </h3>
+              </div>
+              <table className="table">
+                <thead>
+                  <th>Test</th>
+                  <th>Test</th>
+                  <th>Test</th>
+                  <th>Test</th>
+                </thead>{" "}
+                <tbody>
+                  <tr>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                  </tr>
+                </tbody>
+              </table>
+            </>
+          )}
         </div>
       </div>
-
     );
   }
 }
